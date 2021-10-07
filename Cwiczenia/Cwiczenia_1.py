84c92e539bc831463a556fba182ca5e09997792c
monika <monikamarianowakowska@gmail.com>
2021-10-07 11:02:08 +0200

    cwiczenie 1,3

--- Cwiczenia/Cwiczenia_1.py
+++ Cwiczenia/Cwiczenia_1.py
@@ -17,3 +17,26 @@ lorem_ipsum = " Czym jest Lorem Ipsum?" \
 imie="Monika"
 nazwisko="Nowakowska"
 #o co chodzi #TODO dowiedziedz sie o co chodzi
+
+#Zad 3 w format_ciagow.py
+
+
+#Zad 4
+zmienna_typu_string=lorem_ipsum
+print(dir(zmienna_typu_string))
+help(zmienna_typu_string.capitalize) #ctrl + prawy klawisz przenosi do kodu zrodlowego funckji
+
+#Zad 5
+
+i=imie[::-1]
+n=nazwisko[::-1]
+
+print(i.capitalize(), n.capitalize())
+
+#Zad 6
+
+lista=[1,2,3,4,5,6,7,8,9,10]
+
+lista1=[i[1] for i in lista]
+lista2=[i[2]for i in lista]
+print(str(lista1), str(lista2))
